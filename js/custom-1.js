6f87c333c7e930ba129e9031cf4275c4cc6179ac
yaolingyaojiu <15134011019@163.com>
2026-03-15 23:31:46 +0800

    Site updated: 2026-03-15 23:31:46

--- js/custom-1.js
+++ js/custom-1.js
@@ -1,31 +1,32 @@
-function runningTime() {
+function runningTime(){
 
-  const infoBox = document.querySelector('.site-info')
-  if (!infoBox) return
+  const pv = document.querySelector("#busuanzi_value_site_pv");
+  if(!pv) return;
 
-  if (document.querySelector('.running-time')) return
+  if(document.querySelector(".running-time")) return;
 
-  const div = document.createElement('div')
-  div.className = 'running-time'
-  infoBox.appendChild(div)
+  const span = document.createElement("span");
+  span.className = "running-time";
 
-  const start = new Date('2024-01-01 00:00:00').getTime()
+  pv.parentNode.appendChild(span);
 
-  function update() {
+  const start = new Date("2024-01-01 00:00:00").getTime();
 
-    const diff = Date.now() - start
+  function update(){
 
-    const d = Math.floor(diff / 86400000)
-    const h = Math.floor(diff % 86400000 / 3600000)
-    const m = Math.floor(diff % 3600000 / 60000)
-    const s = Math.floor(diff % 60000 / 1000)
+    const diff = Date.now() - start;
 
-    div.innerHTML = `本站已运行 ${d} 天 ${h} 小时 ${m} 分 ${s} 秒`
+    const d = Math.floor(diff/86400000);
+    const h = Math.floor(diff%86400000/3600000);
+    const m = Math.floor(diff%3600000/60000);
+    const s = Math.floor(diff%60000/1000);
+
+    span.innerHTML = ` | 本站已运行 ${d} 天 ${h} 小时 ${m} 分 ${s} 秒`;
   }
 
-  update()
-  setInterval(update,1000)
+  update();
+  setInterval(update,1000);
 }
 
-document.addEventListener("DOMContentLoaded", runningTime)
-document.addEventListener("pjax:complete", runningTime)
+document.addEventListener("DOMContentLoaded", runningTime);
+document.addEventListener("pjax:complete", runningTime);
